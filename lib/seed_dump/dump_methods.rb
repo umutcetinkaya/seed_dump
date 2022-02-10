0ad10360a58f050eed1a952ea9bd0f076edec096
Abdulkadir Belke <a.belke@vispera.co>
2022-02-10 10:42:31 +0300

    final

--- lib/seed_dump/dump_methods.rb
+++ lib/seed_dump/dump_methods.rb
@@ -69,11 +69,9 @@ class SeedDump
 
     def write_records_to_io(records, io, options)
       options[:exclude] ||= [:id, :created_at, :updated_at]
-      puts "#2"
 
       method = options[:import] ? 'import' : 'create!'
       io.write("#{model_for(records)}.#{method}(")
-      puts "#3"
 
       if options[:import]
         io.write("[#{attribute_names(records, options).map {|name| name.to_sym.inspect}.join(', ')}], ")
@@ -85,17 +83,13 @@ class SeedDump
                            else
                              :enumerable_enumeration
                            end
-      puts "#5"
-        puts records                      
       send(enumeration_method, records, io, options) do |record_strings, last_batch|
         io.write(record_strings.join(",\n  "))
 
         io.write(",\n  ") unless last_batch
       end
-      puts "#6"
 
       io.write("\n]#{active_record_import_options(options)})\n")
-      puts "#7"
 
       if options[:file].present?
         nil
@@ -117,7 +111,6 @@ class SeedDump
                         else
                           records[0].attribute_names
                         end
-      puts "#4"
 
       attribute_names.select {|name| !options[:exclude].include?(name.to_sym)}
     end
